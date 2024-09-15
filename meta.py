fac7bacdd85f767956ade37764ae31fa4b38e2f3
Marcin Antas <antas.marcin@gmail.com>
2024-09-15 17:37:32 +0200

    Add support for GLiNER models

--- meta.py
+++ meta.py
@@ -1,12 +1,16 @@
+import json
 from transformers import AutoConfig
 
 class Meta:
-    config: AutoConfig
-
-    def __init__(self, model_path):
-        self.config = AutoConfig.from_pretrained(model_path)
+    def __init__(self, model_path: str, model_name: str):
+        if model_name.startswith("urchade"):
+            with open(f"{model_path}/model_config.json", "r") as f:
+                self.config = json.load(f)
+        else:
+            config = AutoConfig.from_pretrained(model_path)
+            self.config = config.to_dict()
 
     def get(self):
         return {
-            'model': self.config.to_dict()
+            'model': self.config
         }
